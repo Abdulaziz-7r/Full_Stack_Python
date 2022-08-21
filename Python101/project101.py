7f52c302247e98e002592bf0cf9cf63d62d16c9d
ZezoHR <iabdulazizalharthi@gmail.com>
2022-08-21 15:43:41 +0300

    Project done

---
+++ Python101/project101.py
@@ -0,0 +1,23 @@
+#directory that has all telephone numbers and their owners
+telephone_directory =  {
+'1111111111': 'Amal',
+'2222222222': 'Mohammed',
+'3333333333': 'Khadijah',
+'4444444444': 'Abdullah',
+'5555555555': 'Rawan',
+'6666666666': 'Faisal',
+'7777777777': 'Layla'
+}
+
+#function that searches for a telephone number owner's name from the directory
+def find_owner(phone_number, telephone_directory):
+    if (len(phone_number) == 10 and phone_number.isnumeric()):
+        if phone_number in telephone_directory:
+            print ('The owner is:', telephone_directory[phone_number])
+        else:
+            print('Sorry, the number is not found')
+    else:
+        print('This is invalid number')
+
+#asking the user to enter the telephone number
+find_owner(input('Enter telephone number to know the owner: '), telephone_directory)
