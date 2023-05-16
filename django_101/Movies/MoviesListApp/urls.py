0fc54736cdbb48756969180898db984782b7a71c
ZezoHR <iabdulazizalharthi@gmail.com>
2023-05-16 17:58:33 +0300

    Adding model form for publisher table

--- django_101/Movies/MoviesListApp/urls.py
+++ django_101/Movies/MoviesListApp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.Movies_list, name='Movies_list'),
-
+    path("publishers/<int:pk>/", views.publisher_edit, name='publisher_edit'),
+    path("publishers/new/", views.publisher_edit, name='publisher_create'),
 ]
--- django_101/Movies/MoviesListApp/views.py
+++ django_101/Movies/MoviesListApp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from .models import Movie_info
+from django.shortcuts import redirect, render, get_object_or_404
+from .models import Movie_info, Publisher
 from .utils import avg_rate
+from .forms import PublisherForm
+from django.contrib import messages
 
 def Movies_list(request):
     movies = Movie_info.objects.all()
@@ -17,3 +19,22 @@ def Movies_list(request):
     
     context = {'Movies_list' : movies_list}
     return render(request, 'MoviesListApp/Movies_list.html', context)
+
+def publisher_edit(request, pk=None):
+    if pk is not None:
+        publisher = get_object_or_404(Publisher, pk= pk)
+    else: 
+        publisher = None
+    
+    if request.method == "POST":
+        form = PublisherForm(request.POST, instance=publisher)
+        if form.is_valid():
+            update_publisher = form.save()
+            if publisher is None:
+                messages.success(request, 'Publisher "{}" was created'.format(update_publisher))
+            else:
+                messages.success(request, 'Publisher "{}" was updated'.format(update_publisher))
+            return redirect('publisher_edit', update_publisher.pk)
+    else:
+        form = PublisherForm(instance=publisher)
+    return render(request, 'forms.html', {"method": request.method, "form": form})
